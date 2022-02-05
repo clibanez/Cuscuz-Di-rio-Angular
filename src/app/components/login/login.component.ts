3a0b5982c2d3b9af71ac291d8fc81c5d334bf4ba
Clibanez <clibanezzcaldas@gmail.com>
2022-02-05 11:48:50 -0300

    login implementado

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from '../../models/credenciais';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,31 +13,33 @@ import { Credenciais } from '../../models/credenciais';
 export class LoginComponent implements OnInit {
 
   creds: Credenciais = {
-    email:'',
+    email: '',
     senha: ''
   }
 
-  email = new FormControl(null,Validators.email);
-  senha = new FormControl(null,Validators.minLength(3));
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
 
-  constructor(private toast: ToastrService) { }
+  constructor(
+    private toast: ToastrService,
+    private service: AuthService,
+    private router: Router) { }
+
+  ngOnInit(): void { }
+
+  logar() {
+    this.service.authenticate(this.creds).subscribe(resposta => {
+      this.service.successfulLogin(resposta.headers.get('Authorization').substring(7));
+      this.router.navigate([''])
+    }, () => {
+this.toast.error('Usuário e/ou senha inválidos');
+    })
+}
 
 
-  ngOnInit(): void {
-  }
 
-  logar(){
-    this.toast.error('Usuário e/ou senha inválidos!', 'Login');
-    this.creds.senha = '';
-
-  }
-
-  validaCampos(): boolean{
-    if(this.email.valid && this.senha.valid){
-      return true;
-    }else{
-      return false;
-    }
+  validaCampos(): boolean {
+    return this.email.valid && this.senha.valid
   }
 
 }
